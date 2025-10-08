0f1ecc58673ec4bad0fb575aade38e66b98c01e6
Alph702 <alph702.12@gmail.com>
2025-10-08 17:18:41 +0500

    learned functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    my_function(6);
+    print_labeled_measurement(5, 'h');
+
+    let sqrt = sqrt(2);
+    println!("The sqrt of 2 is: {sqrt}")
+
+}
+
+fn my_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit: char) {
+    println!("The measurment is: {value}{unit}")
+}
+
+fn sqrt(x: i32) -> i32 {
+    x * x
 }
