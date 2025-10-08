85d7915bd5e68d20835398a32e598af2be9127e2
Alph702 <alph702.12@gmail.com>
2025-10-08 17:29:43 +0500

    some thing

---
+++ HelloWorld/target/debug/deps/HelloWorld-911a0e183fe7b37f.d
@@ -0,0 +1,5 @@
+E:\Code\Rust\HelloWorld\target\debug\deps\HelloWorld-911a0e183fe7b37f.d: src\main.rs
+
+E:\Code\Rust\HelloWorld\target\debug\deps\libHelloWorld-911a0e183fe7b37f.rmeta: src\main.rs
+
+src\main.rs:
